d9cf3c483f657ad278eb9da79722e14c75efc209
Takosuke <solidparallel6890@gmail.com>
2016-05-19 15:02:09 +0200

    building main app

--- html/main/views.py
+++ html/main/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from blog.models import Post
 
 def index(request):
+    latest_posts = Post.query.all()[:10]
     template = loader.get_template('main/index.html')
     context = {
+            'latest_posts' : latest_posts
     }
     return HttpResponse(template.render(context, request))
 
