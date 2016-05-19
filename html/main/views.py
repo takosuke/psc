159f38e383ae7926210d45730161fae36b1b188e
Takosuke <solidparallel6890@gmail.com>
2016-05-19 15:28:03 +0200

    removed bower components again

--- html/main/views.py
+++ html/main/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 from blog.models import Post
 
 def index(request):
-    latest_posts = Post.query.all()[:10]
+    latest_posts = Post.objects.all()[:10]
     template = loader.get_template('main/index.html')
     context = {
             'latest_posts' : latest_posts
